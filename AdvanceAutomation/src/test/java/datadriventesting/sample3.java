b0164ee643b57e736e2f0a9e5e87c5a57fae4bf0
shaik shabana <shaik shabana@DESKTOP-C3D4EMT>
2024-08-13 11:22:00 +0530

    commited by sony

--- AdvanceAutomation/src/test/java/datadriventesting/sample3.java
+++ AdvanceAutomation/src/test/java/datadriventesting/sample3.java
@@ -5,6 +5,7 @@ public class sample3 {
 	public static void main(String[] args) {
 		 System.out.println("heloooo");
 		 System.out.println("hii.......");
+		 System.out.println("hello.......");
 
 	}
 
